0577938a673e0819de95f79346b7bed6ca06de08
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2026-02-02 11:15:58 +0530

    improved ass 3 q1

--- Assignment-03/q-1.py
+++ Assignment-03/q-1.py
@@ -40,30 +40,109 @@
 #         location = move(location, action)
 
 # Type-2 : user can give percepts
-import random
+# import random
 
-def vacuum_agent(location, status):
-    if status == "Dirt":
-        return "Remove"
+# def vacuum_agent(location, status):
+#     if status == "Dirt":
+#         return "Remove"
 
-    if location == "A":
-        return "Move Right"
+#     if location == "A":
+#         return "Move Right"
 
-    if location == "C":
-        return "Move Left"
+#     if location == "C":
+#         return "Move Left"
 
-    if location == "B":
-        return random.choice(["Move Left", "Move Right"])
+#     if location == "B":
+#         return random.choice(["Move Left", "Move Right"])
 
 
-steps = int(input("Enter number of percepts: "))
+# steps = int(input("Enter number of percepts: "))
 
-print("\nPercept\t\tAction")
-print("-------------------------------")
+# print("\nPercept\t\tAction")
+# print("-------------------------------")
 
-for _ in range(steps):
-    location = input("Enter Location (A/B/C): ")
-    status = input("Enter Status (Dirt/No Dirt): ")
+# for _ in range(steps):
+#     location = input("Enter Location (A/B/C): ")
+#     status = input("Enter Status (Dirt/No Dirt): ")
 
-    action = vacuum_agent(location, status)
-    print(f"({location}, {status})\t{action}")
+#     action = vacuum_agent(location, status)
+#     print(f"({location}, {status})\t{action}")
+# type-3
+class Env:
+
+    def __init__(self):
+        self.cond = {
+            "a": "dirty",
+            "b": "dirty",
+            "c": "dirty"
+        }
+
+
+class Agent:
+
+    def __init__(self):
+        self.loc = "a"
+        self.cost = 0
+
+        self.rule = {
+            ("a", "dirty"): "vaccuum work",
+            ("a", "clean"): "move right",
+            ("b", "dirty"): "vaccuum work",
+            ("b", "clean"): "move right",
+            ("c", "dirty"): "vaccuum work",
+            ("c", "clean"): "move left"
+        }
+
+        self.score = {
+            "vaccuum work": 5,
+            "move right": 1,
+            "move left": 1,
+            "no_op": 0
+        }
+
+    def next(self, curr, action):
+        if action == "move right":
+            if curr == "a":
+                return "b"
+            elif curr == "b":
+                return "c"
+            else:
+                return "c"
+
+        elif action == "move left":
+            if curr == "c":
+                return "b"
+            elif curr == "b":
+                return "a"
+            else:
+                return "a"
+
+        return curr
+
+    def act(self, env):
+
+        status = env.cond[self.loc]
+        percept = (self.loc, status)
+        action = self.rule[percept]
+
+        print(percept, "\t", action, "\t ", self.loc)
+
+        if action == "vaccuum work":
+            env.cond[self.loc] = "clean"
+            self.cost += self.score[action]
+        else:
+            self.loc = self.next(self.loc, action)
+            self.cost += self.score[action]
+
+env = Env()
+agent = Agent()
+
+steps = 10
+
+print("Percept\t\tAction\t\tLocation\tRoom Conditions")
+
+for step in range(steps):
+    agent.act(env)
+    print("\t\t\t\t\t", env.cond)
+
+print("\nTotal Cost:", agent.cost)
