a4892185e36ef9d478d5f4a216b382ed0f75ea52
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2026-02-16 11:20:59 +0530

    added asssignment-05

--- Assignment-05/q-1_q-2.py
+++ Assignment-05/q-1_q-2.py
@@ -1,123 +1,131 @@
-def is_valid(g, b):
-    # out of bounds
-    if g < 0 or b < 0 or g > 3 or b > 3:
+class NODE:
+    def __init__(self, state, parent=None):
+        self.STATE = state
+        self.PARENT = parent
+        self.DEPTH = 0 if parent is None else parent.DEPTH + 1
+
+
+
+class Problem:
+    def __init__(self):
+        self.INITIAL = (3, 3, 0)   # (girls_left, boys_left, boat_position)
+        self.GOAL = (0, 0, 1)
+
+    def IS_GOAL(self, state):
+        return state == self.GOAL
+
+
+
+def IS_VALID(g_left, b_left):
+    g_right = 3 - g_left
+    b_right = 3 - b_left
+
+    if not (0 <= g_left <= 3 and 0 <= b_left <= 3):
         return False
 
-    # right bank people
-    g_r = 3 - g
-    b_r = 3 - b
-
-    # left bank unsafe
-    if g > 0 and b > g:
+    if g_left > 0 and b_left > g_left:
         return False
 
-    # right bank unsafe
-    if g_r > 0 and b_r > g_r:
+    if g_right > 0 and b_right > g_right:
         return False
 
     return True
 
-moves = [(1,0),(2,0),(0,1),(0,2),(1,1)]
-
-def successors(state):
-    g, b, boat = state
-    next_states = []
-
-    for mg, mb in moves:
-        if boat == 0:  # boat on left → go right
-            new = (g-mg, b-mb, 1)
-        else:          # boat on right → go left
-            new = (g+mg, b+mb, 0)
-
-        if is_valid(new[0], new[1]):
-            next_states.append(new)
-
-    return next_states
-
-# Depth-Limited Search
-print("\nDepth-Limited Search:-")
-explored_dls = 0
-
-def DLS(state, goal, limit, path, visited):
-    global explored_dls
-    explored_dls += 1
-
-    if state == goal:
-        return path + [state]
-
-    if limit == 0:
-        return None
-
-    visited.add(state)
-
-    for child in successors(state):
-        if child not in visited:
-            result = DLS(child, goal, limit-1, path+[state], visited)
-            if result:
-                return result
-
-    visited.remove(state)
-    return None
-
-start = (3,3,0)
-goal = (0,0,1)
-print("\nfor limit=3")
-solution_dls = DLS(start, goal,3, [], set())
-
-print("DLS Solution:", solution_dls)
-print("States Explored (DLS):", explored_dls)
-print("\nfor limit=10")
-solution_dls = DLS(start, goal,10, [], set())
-
-print("DLS Solution:", solution_dls)
-print("States Explored (DLS):", explored_dls)
-print("\nfor limit=15")
-solution_dls = DLS(start, goal,15, [], set())
-
-print("DLS Solution:", solution_dls)
-print("States Explored (DLS):", explored_dls)
-
-# Iterative Deepening Search
-print("\nIterative Deepening Search:-")
-def IDS(start, goal, max_depth=20):
-    global explored_ids
-    explored_ids = 0
-
-    for depth in range(max_depth):
-        visited=set()
-        result = IDS_DLS(start, goal, depth, [], visited)
-        if result:
-            return result, depth
-
-    return None, None
 
 
-def IDS_DLS(state, goal, limit, path, visited):
-    global explored_ids
-    explored_ids += 1
+def EXPAND(problem, node):
+    g_left, b_left, boat = node.STATE
+    moves = [(1,0),(2,0),(0,1),(0,2),(1,1)]
+    children = []
 
-    if state == goal:
-        return path + [state]
+    for g, b in moves:
+        if boat == 0:
+            new_state = (g_left - g, b_left - b, 1)
+        else:
+            new_state = (g_left + g, b_left + b, 0)
 
-    if limit == 0:
-        return None
+        if IS_VALID(new_state[0], new_state[1]):
+            children.append(NODE(new_state, node))
 
-    visited.add(state)
+    return children
 
-    for child in successors(state):
-        if child not in visited:
-            result = IDS_DLS(child, goal, limit-1, path+[state], visited)
-            if result:
-                return result
+def IS_CYCLE(node):
+    current = node.PARENT
+    while current:
+        if current.STATE == node.STATE:
+            return True
+        current = current.PARENT
+    return False
 
-    visited.remove(state)
-    return None
+def DEPTH(node):
+    return node.DEPTH
 
-solution_ids, depth = IDS(start, goal)
 
-print("\nIDS Solution Path:")
-for s in solution_ids:
-    print(s)
+def DLS(problem,l):
+    frontier = []
+    frontier.append(NODE(problem.INITIAL))
+    global dls_count
+    dls_count = 0
 
-print("Solution Depth:", depth)
-print("States Explored (IDS):", explored_ids)
+    result = "FAILURE"
+
+    while frontier :
+        node = frontier.pop()
+
+
+        if problem.IS_GOAL(node.STATE):
+            return node
+        
+        if DEPTH(node) > l:
+            result = "CUTOFF"
+
+        elif not IS_CYCLE(node):
+            dls_count += 1
+            
+            for child in EXPAND(problem,node):
+                frontier.append(child)
+    return result
+
+
+problem = Problem()
+
+print("Depth Limited Search (limit = 3):")
+dls_result = DLS(problem, 3)
+print(dls_result)
+print("Number of nodes explored:")
+print(dls_count)
+
+def IDPS(problem):
+    global depth
+    depth = 0
+    global idps_count
+    idps_count = 0
+    while True:
+        result = DLS(problem, depth)
+        idps_count += dls_count
+        
+        if result != "CUTOFF":
+            return result
+        
+        depth += 1
+
+
+
+
+print("\nIterative Deepening Search:")
+ids_result = IDPS(problem)
+
+if isinstance(ids_result, NODE):
+    print("Solution found at depth:", ids_result.DEPTH)
+
+    path = []
+    while ids_result:
+        path.append(ids_result.STATE)
+        ids_result = ids_result.PARENT
+    path.reverse()
+    print(path)
+else:
+    print(ids_result)
+
+print("Number of nodes explored:")
+print(idps_count)
